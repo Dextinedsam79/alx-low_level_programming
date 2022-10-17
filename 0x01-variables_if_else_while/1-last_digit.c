26a9c3ac697d49c419c247eb0adb0983e3fada46
Dextinedsam79 <dextinedsam93@gmail.com>
2022-10-17 12:55:49 -0700

    C-program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,34 @@
-#include <stdlib.h> 
-#include <time.h> 
-#include <stdio.h> 
-  
- /** 
-  * main - A random number is assigned to variable n 
-  * The last digit of the number is checked to see if it is greater than 5 
-  * The number, digit and the result of the evaluation is printed. 
-  * Return: ALways 0 (success) 
-  */ 
- int main(void) 
- { 
-         int n, r; 
-  
-         srand(time(0)); 
-         n = rand() - RAND_MAX / 2; 
-         r = n % 10; 
-         printf("Last digit of %d is %d ", n, r); 
-         if (r > 5) 
-                 printf("and is greater than 5\n"); 
-         else if (r == 0) 
-                 printf("and is 0\n"); 
-         else if (r < 6 && r != 0) 
-                 printf("and is less than 6 and not 0\n"); 
-         return (0); 
- }
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * * main - print if the number is postive, zero, or negative
+ * *
+ * * Description: using the main function
+ * * this program prints Programming is positive, zero, or negative
+ * * Return: 0
+ */
+
+int main(void)
+{
+int n;
+int l;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+l = n % 10;
+
+if (l > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, l);
+}
+else if (l == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, l);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+}
+return (0);
+}
