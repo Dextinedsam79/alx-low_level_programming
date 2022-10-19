2356c25ee924628533c59d99a4268928adb6d815
dextinedsam79 <dextinedsam93@gmail.com>
2022-10-18 20:59:02 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
  * main - prints _putchar and a new line
- * Return: Always 0
+ * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
